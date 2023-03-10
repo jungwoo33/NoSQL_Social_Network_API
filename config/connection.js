e34e371fd15701c80bda3e401d1b7ca2ad6ff77f
Jungwoo Lee <jungwoo33@gmail.com>
2023-03-10 16:30:19 -0600

    bug fixing started

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1/NoSQL_Social_Network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
