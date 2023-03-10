9fb8fb135500bce8856af10c48bfa694d6cba316
Jungwoo Lee <jungwoo33@gmail.com>
2023-03-10 15:17:36 -0600

    api/thought_routes.js has been updated

---
+++ routes/api/thought_routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+   get_all_thoughts,
+   get_thought_by_id,
+   post_new_thought,
+   update_thought_by_id,
+   delete_thought_by_id,
+   add_reaction,
+   delete_reaction
+} = require('../../controllers/thought_controller');
+
+router.route('/')
+   .get(get_all_thoughts)
+   .post(post_new_thought);
+router.route('/:id')
+   .get(get_thought_by_id)
+   .put(update_thought_by_id)
+   .delete(delete_thought_by_id);
+router.route('/:thoughtId/reactions')
+   .post(add_reaction);
+router.route('/:thoughtId/:reactionId')
+   .delete(delete_friend);
+
+module.exports = router;
