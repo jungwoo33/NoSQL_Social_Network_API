ad973906cfe4b27087e6f653442621d64220c676
Jungwoo Lee <jungwoo33@gmail.com>
2023-03-10 15:09:27 -0600

    api/user_routes.js has been updated

---
+++ routes/api/user_routhes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+   get_all_users,
+   get_user_by_id,
+   post_new_user,
+   update_user_by_id,
+   delete_user_by_id,
+   add_friend,
+   delete_friend
+} = require('../../controllers/user_controller');
+
+router.route('/')
+   .get(get_all_users)
+   .post(post_new_user);
+router.route('/:id')
+   .get(get_user_by_id)
+   .put(update_user_by_id)
+   .delete(delete_user_by_id);
+router.route('/:userId/friends/:friendId')
+   .post(add_friend)
+   .delete(delete_friend);
+
+module.exports = router;
